4a48ce8d05e71c2d797d76b28970b519216c5504
hamada202416 <yri2.9mk8@gmail.com>
2024-11-22 00:34:07 +0900

    login.blade.phpを編集

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>ログイン画面</title>
+    <title>書籍管理システム</title>
 </head>
 <body>
-    <h1>ログイン</h1>
+    <h1>書籍管理システム</h1>
+    <h2>ログイン</h2>
     ログインID：<input type="text" name="loginId">
     パスワード：<input type="password" name="password">
     <input type="submit" value="ログイン">
