5b9bc14a3a88c92f4a20504fc0e6cefa582911fc
ICT18 <ict18@example.com>
2024-11-22 21:30:33 +0900

    ログイン画面以外のページに上下猫の画像が表示されるようにしました。

--- resources/views/review_update.blade.php
+++ resources/views/review_update.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/topBottom.css"> <!-- CSSファイルをリンク -->
     <title>レビュー更新画面</title>
 </head>
 <body>
