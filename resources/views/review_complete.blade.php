b1c8a1603b76dfdce7967b453c941a613f70379f
hamada202416 <yri2.9mk8@gmail.com>
2024-11-22 14:04:21 +0900

    背景色が表示されるように修正

--- resources/views/review_complete.blade.php
+++ resources/views/review_complete.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/topBottom.css"> <!-- CSSファイルをリンク -->
+    <link rel="stylesheet" href="{{ asset('css/topBottom.css') }}"> <!-- CSSファイルをリンク -->
     <title>登録完了</title>
 </head>
 <body>
--- resources/views/review_update.blade.php
+++ resources/views/review_update.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/topBottom.css"> <!-- CSSファイルをリンク -->
+    <link rel="stylesheet" href="{{ asset('css/topBottom.css') }}"> <!-- CSSファイルをリンク -->
     <title>レビュー更新画面</title>
 </head>
 <body>
