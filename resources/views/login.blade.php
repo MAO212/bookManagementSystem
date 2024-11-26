7d8312147bad99d4bd001795b36b9f97c9793d61
yamagishi <mautaso.08@gmail.com>
2024-11-26 13:38:11 +0900

    login画面のviewのテスト

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,16 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>書籍管理システム</title>
+    <link rel="stylesheet" href="styles.css"> <!-- CSSファイルをリンク -->
 </head>
 <body>
-    <h1>書籍管理システム</h1>
-    <h2>ログイン</h2>
-    <form action="/login" method="post">
-        @csrf
-        ユーザーID : <input type="text" name="userId" required>
-        パスワード : <input type="password" name="password" required>
-        <input type="submit" value="ログイン">
-        <input type="reset" value="リセット">
-    </form>
+    <div class="login-container">
+        <h1>書籍管理システム</h1>
+        <h2>ログイン</h2>
+        <form action="/login" method="post">
+            @csrf
+            <input type="text" name="userId" placeholder="ユーザーID" required>
+            <input type="password" name="password" placeholder="パスワード" required>
+            <a href="#" class="button">ログイン</a>
+            <input type="reset" value="リセット" class="reset-button">
+        </form>
+    </div>
 </body>
 </html>
