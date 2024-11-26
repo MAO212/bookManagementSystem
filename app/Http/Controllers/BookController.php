727e6f732b8796afc9ea3b9a0e3f7c8ca8712276
yamakake2580 <yamamoto231231@gmail.com>
2024-11-26 11:36:44 +0900

    Bookcontrollerの編集

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Book; // Bookモデルをインポート
+use App\Model\Employee; // Employeeモデルをインポート
+use App\Models\Review; // Reviewモデルをインポート
 
 class BookController extends Controller
 {
-    //
+    public function index()
+    {
+        // データベースから書籍データを取得
+        $books = Book::all(); // 全ての書籍を取得
+
+        // ビューにデータを渡す
+        return view('index', ['books' => $books]);
+    }
+    
 }
