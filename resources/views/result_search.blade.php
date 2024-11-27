9fe0eb4e98788a3af2dcc7edf6d2ec8925da2cec
ICT18 <ict18@example.com>
2024-11-27 21:53:20 +0900

    背景画像の追加、レイアウトを調整しました。

--- resources/views/result_search.blade.php
+++ resources/views/result_search.blade.php
@@ -4,12 +4,42 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/topBottom.css') }}"> <!-- CSSファイルをリンク -->
+    <title>書籍検索結果</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 60px;
+            line-height: 1.6;
+        }
+        h1{
+            color: #333c5e;
+            background-color:white;
+            border-left: 20px solid #C58D98;
+            border-right: 20px solid #C58D98;
+            border-bottom: 2px solid #C58D98;
+        }
+        .btn-primary {
+            background-color: #B0C4DE;
+            color: white;
+            border: none;
+            font-size: 14px; /* サイズを小さく */
+            padding: 5px 10px; /* パディングを調整 */
+        }
+        .btn-primary:hover {
+            background-color: #E6B6B6;
+        }
+    </style>
+
 </head>
 <body>
     {{-- {{dd($bookData);}} --}}
     @if(isset($bookData))
-            <h2>書籍情報</h2>
+            <h1>書籍情報</h1>
             <p>ISBN: {{ $bookData['isbn'] }}</p>
             <p>書籍名: {{ $bookData['title'] }}</p>
             <p>著者名:
@@ -20,7 +50,7 @@
                 @endif
             </p>
             <p>出版社名: {{ $bookData['publisher'] ?? '不明' }}</p>
-            <p>価格: {{ isset($bookData['price']) ? $bookData['price'] : '不明' }}</p>
+            <p>価格: ￥{{ isset($bookData['price']) ? $bookData['price'] : '不明' }}</p>
             <p>
                 @if(isset($bookData['imageLinks']['thumbnail']))
                     <img src="{{ $bookData['imageLinks']['thumbnail'] }}" alt="書籍画像" style="max-width: 100px;">
