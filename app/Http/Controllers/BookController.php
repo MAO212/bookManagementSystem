ba0ee109faa388a9dcfcfaaf55ce7484000c8c55
yamakake2580 <yamamoto231231@gmail.com>
2024-11-28 09:45:31 +0900

    レビューの投稿機能の再実装

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -118,7 +118,8 @@ class BookController extends Controller
             ->with([
                 'name' => Session::get('user')->name, // 投稿者名
                 'post_content' => $review->post_content, // レビュー本文
-                'score' => $review->score // 点数
+                'score' => $review->score, // 点数
+                'book_id' => $review->book_id // 書籍IDを追加
             ]);
     }
 
@@ -128,8 +129,9 @@ class BookController extends Controller
         $name = $req->session()->get('name');
         $post_content = $req->session()->get('post_content');
         $score = $req->session()->get('score');
+        $book_id = $req->session()->get('book_id'); // 書籍IDを取得
 
-        return view('review_complete', compact('name','post_content','score'));
+        return view('review_complete', compact('name','post_content','score', 'book_id'));
     }
 
     public function book_register()
